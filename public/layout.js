ef47f13dcd30b03fe7500dd02cd8bb902e9d8c16
Beamsara <beamlovelove112@gmail.com>
2022-08-31 16:39:46 +0700

    EMM

--- public/layout.js
+++ public/layout.js
@@ -1,3 +1,17 @@
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCJM-ENRBlwTR5hyAqFduxJ2Xyzm82Pud0",
+//   authDomain: "esm-system.firebaseapp.com",
+//   databaseURL: "https://esm-system-default-rtdb.firebaseio.com",
+//   projectId: "esm-system",
+//   storageBucket: "esm-system.appspot.com",
+//   messagingSenderId: "601934263236",
+//   appId: "1:601934263236:web:cb0490398c1a0c3432ea91",
+//   measurementId: "G-WZLEMCTJ9E"
+// };
+// firebase.initializeApp(firebaseConfig);
+
+
 (function() {
   'use strict';
 
@@ -61,14 +75,14 @@ function w3_close() {
 function on_off_led1() {
   var x = document.getElementById("tog1");
   
-
   if (x.innerHTML === "ปิด") {
     x.innerHTML = "เปิด";
     document.getElementById("theImage1").src = "ledon.png"
+    document.getElementById("theImage11").src = "ledon.png"
   } else {
     x.innerHTML = "ปิด";
     document.getElementById("theImage1").src = "ledoff.png"
-
+    document.getElementById("theImage11").src = "ledoff.png"
   }
 }
 
@@ -79,9 +93,11 @@ function on_off_led2() {
   if (x.innerHTML === "ปิด") {
     x.innerHTML = "เปิด";
     document.getElementById("theImage2").src = "ledon.png"
+    document.getElementById("theImage22").src = "ledon.png"
   } else {
     x.innerHTML = "ปิด";
     document.getElementById("theImage2").src = "ledoff.png"
+    document.getElementById("theImage22").src = "ledoff.png"
 
   }
   
@@ -93,9 +109,11 @@ function on_off_led3() {
   if (x.innerHTML === "ปิด") {
     x.innerHTML = "เปิด";
     document.getElementById("theImage3").src = "ledon.png"
+    document.getElementById("theImage33").src = "ledon.png"
   } else {
     x.innerHTML = "ปิด";
     document.getElementById("theImage3").src = "ledoff.png"
+    document.getElementById("theImage33").src = "ledoff.png"
 
   }
   
@@ -132,9 +150,11 @@ function on_off_plug1() {
   if (x.innerHTML === "ปิด") {
     x.innerHTML = "เปิด";
     document.getElementById("theImagep1").src = "plugon.png"
+    document.getElementById("theImagep11").src = "plugon.png"
   } else {
     x.innerHTML = "ปิด";
     document.getElementById("theImagep1").src = "plugoff.png"
+    document.getElementById("theImagep11").src = "plugoff.png"
 
   }
 }
@@ -146,9 +166,11 @@ function on_off_plug2() {
   if (x.innerHTML === "ปิด") {
     x.innerHTML = "เปิด";
     document.getElementById("theImagep2").src = "plugon.png"
+    document.getElementById("theImagep22").src = "plugon.png"
   } else {
     x.innerHTML = "ปิด";
     document.getElementById("theImagep2").src = "plugoff.png"
+    document.getElementById("theImagep22").src = "plugoff.png"
 
   }
 }
@@ -160,9 +182,69 @@ function on_off_plug3() {
   if (x.innerHTML === "ปิด") {
     x.innerHTML = "เปิด";
     document.getElementById("theImagep3").src = "plugon.png"
+    document.getElementById("theImagep33").src = "plugon.png"
   } else {
     x.innerHTML = "ปิด";
     document.getElementById("theImagep3").src = "plugoff.png"
+    document.getElementById("theImagep33").src = "plugoff.png"
+
+  }
+}
+
+function myAccFunc3() {
+  var x = document.getElementById("demoAcc3");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+document.getElementById("myBtn3").click();
+
+function on_off_wire1() {
+
+  
+
+  if (x.innerHTML === "ผิดปกติ") {
+    x.innerHTML = "ปกติ";
+    document.getElementById("theImagew1").src = "wireon.png"
+    document.getElementById("theImagew11").src = "wireon.png"
+    
+  } else {
+    x.innerHTML = "ผิดปกติ";
+    document.getElementById("theImagew1").src = "wireoff.png"
+    document.getElementById("theImagew11").src = "wireoff.png"
+
+  }
+}
+function on_off_wire2() {
+  var x = document.getElementById("togw2");
+  
+
+  if (x.innerHTML === "ผิดปกติ") {
+    x.innerHTML = "ปกติ";
+    document.getElementById("theImagew2").src = "wireon.png"
+    document.getElementById("theImagew22").src = "wireon.png"
+  } else {
+    x.innerHTML = "ผิดปกติ";
+    document.getElementById("theImagew2").src = "wireoff.png"
+    document.getElementById("theImagew22").src = "wireoff.png"
+
+  }
+}
+function on_off_wire3() {
+  var x = document.getElementById("togw3");
+  
+
+  if (x.innerHTML === "ผิดปกติ") {
+    x.innerHTML = "ปกติ";
+    document.getElementById("theImagew3").src = "wireon.png"
+    document.getElementById("theImagew33").src = "wireon.png"
+  } else {
+    x.innerHTML = "ผิดปกติ";
+    document.getElementById("theImagew3").src = "wireoff.png"
+    document.getElementById("theImagew33").src = "wireoff.png"
 
   }
 }
